f2a96984fb4550244e3a54852a06314d8ae61117
yamori <gregdelgado@protonmail.com>
2023-03-25 19:31:20 +0800

    complete solution

--- client/index.js
+++ client/index.js
@@ -3,12 +3,17 @@ const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
+const merkleTree = new MerkleTree(niceList);
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
-
+  const name = 'Gloria Fadel';
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
+  
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name, proof
   });
 
   console.log({ gift });
--- utils/example.js
+++ utils/example.js
@@ -9,11 +9,11 @@ const merkleTree = new MerkleTree(niceList);
 const root = merkleTree.getRoot();
 
 // find the proof that norman block is in the list 
-const name = 'Norman Block';
+const name = 'Fred Leannon';
 const index = niceList.findIndex(n => n === name);
 const proof = merkleTree.getProof(index);
 
 // verify proof against the Merkle Root
 console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
-
+console.log(root);
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
